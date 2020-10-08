7709fec07c4444afd8442a760ca5c1ed503e1b92
DriesDD <driesdedecker@gmail.com>
2020-10-08 14:23:01 +0200

    simplify code, make fighting less fatal so defending is more viable

--- evomatrix.py
+++ evomatrix.py
@@ -2,13 +2,13 @@ import Tkinter
 import sched, time, random, math
 
 g_cols, g_rows = 50,50
-g_alive  = [[False for x in range(g_cols)] for y in range(g_rows)]
-g_energy = [[0 for x in range(g_cols)] for y in range(g_rows)]
-g_motion = [[0.3 for x in range(g_cols)] for y in range(g_rows)]
-g_diet   = [[0.3 for x in range(g_cols)] for y in range(g_rows)]
-g_power   = [[0.3 for x in range(g_cols)] for y in range(g_rows)]
+g_alive   = [[False for x in range(g_cols)] for y in range(g_rows)]
+g_energy  = [[0 for x in range(g_cols)] for y in range(g_rows)]
+g_motion  = [[0.5 for x in range(g_cols)] for y in range(g_rows)]
+g_diet    = [[0.8 for x in range(g_cols)] for y in range(g_rows)]
+g_power   = [[0.5 for x in range(g_cols)] for y in range(g_rows)]
 tps = 500
-fps = 10
+fps = 20
 
 root = Tkinter.Tk()
 
@@ -31,7 +31,7 @@ def draw_grid(): #draws every cell
         for y in range(0, g_rows):
             if g_alive[x][y] == True:
                 trim = (cs-(cs*pow(g_energy[x][y],2)))/2
-                cellcolor = '#' + floattohex(g_power[x][y]) + floattohex(1-1*g_diet[x][y]) + floattohex(g_motion[x][y])
+                cellcolor = '#' + floattohex(g_power[x][y]) + floattohex(g_diet[x][y]) + floattohex(g_motion[x][y])
                 c.create_rectangle(x*cs+trim,y*cs+trim,x*cs+cs-trim,y*cs+cs-trim,fill=cellcolor, tag='cell')
     root.after(1000/fps, draw_grid)
 
@@ -39,48 +39,52 @@ def step_grid(): #calculations for behavior of every cell
     for x in range(0, g_cols):
         for y in range(0, g_rows):
             if g_alive[x][y]:
-                #growth and learning
-                g_energy[x][y] += 0.1 * (1 - g_diet[x][y])
-                #movement and fighting
+                #get energy from the sun
+                g_energy[x][y] += 0.1 * (g_diet[x][y])
+                #lose energy
+                g_energy[x][y] -= (0.03+0.02*g_power[x][y])
+                #movement
                 if g_energy[x][y] > 0.2:
                     pick = random.choice(NEIGHBOURLIST)
                     ox = int(x+pick[0]) % g_cols
                     oy = int(y+pick[1]) % g_rows
                     if random.random() < g_motion[x][y]:
-                        if g_alive[ox][oy] == False: #movement
-                            g_alive[ox][oy] = True
-                            g_energy[ox][oy] = g_energy[x][y] - 0.05
+                        if g_alive[ox][oy] == False:
+                            g_energy[ox][oy] = min(1,(g_energy[x][y] - 0.03))
                             g_diet[ox][oy] = g_diet[x][y]
                             g_motion[ox][oy] = g_motion[x][y]
+                            g_power[ox][oy] = g_power[x][y]
                             g_alive[x][y] = False
-                        else: #fighting each other and live organ transplants
-                            if g_energy[ox][oy] > g_power[x][y]: 
-                                g_energy[ox][oy] -= g_power[x][y]
-                                g_energy[x][y] += g_power[x][y] * g_diet[x][y]
+                            g_alive[ox][oy] = True
+                #fight and eat if moving to another cell
+                        else:
+                            if g_energy[ox][oy] > 0.8*g_power[x][y]: 
+                                g_energy[ox][oy] -= 0.8*g_power[x][y]
+                                g_energy[x][y] += 0.8*g_power[x][y] * (1-g_diet[x][y])
                             else:
-                                g_energy[x][y] += g_energy[ox][oy] * g_diet[x][y]
+                                g_energy[x][y] += g_energy[ox][oy] * (1-g_diet[x][y])
                                 g_alive[ox][oy] = False
                             if g_alive[ox][oy] == True: #other fights back
-                                if g_energy[x][y] > g_power[ox][oy]:
-                                    g_energy[x][y] -= g_power[ox][oy]
-                                    g_energy[ox][oy] += g_power[ox][oy] * g_diet[ox][oy]
+                                if g_energy[x][y] > 0.8*g_power[ox][oy]:
+                                    g_energy[x][y] -= 0.8*g_power[ox][oy]
+                                    g_energy[ox][oy] += 0.8*g_power[ox][oy] * (1-g_diet[ox][oy])
                                 else:
-                                    g_energy[ox][oy] += g_energy[x][y] * g_diet[ox][oy]
+                                    g_energy[ox][oy] += g_energy[x][y] * (1-g_diet[ox][oy])
                                     g_alive[x][y] = False
                                 if g_energy[ox][oy] > 1:
                                     g_energy[ox][oy] = 1
-                #the miracle of birth
+                #reproduce
                     if g_energy[x][y] > 0.9:
-                        if g_alive[int(x+pick[0]) % g_cols][int(y+pick[1]) % g_rows] == False:
-                            g_alive[int(x+pick[0]) % g_cols][int(y+pick[1]) % g_rows] = True
-                            g_energy[int(x+pick[0]) % g_cols][int(y+pick[1]) % g_rows] = 0.3
-                            g_diet[int(x+pick[0]) % g_cols][int(y+pick[1]) % g_rows] = mutate(g_diet[x][y])
-                            g_motion[int(x+pick[0]) % g_cols][int(y+pick[1]) % g_rows] = mutate(g_motion[x][y])
-                            g_power[int(x+pick[0]) % g_cols][int(y+pick[1]) % g_rows] = mutate(g_power[x][y])                   
+                        pick = random.choice(NEIGHBOURLIST)
+                        ox = int(x+pick[0]) % g_cols
+                        oy = int(y+pick[1]) % g_rows
+                        if g_alive[ox][oy] == False:
+                            g_alive[ox][oy] = True
+                            g_energy[ox][oy] = 0.3
+                            g_diet[ox][oy]   = mutate(g_diet[x][y])
+                            g_motion[ox][oy] = mutate(g_motion[x][y])
+                            g_power[ox][oy]  = mutate(g_power[x][y])                   
                             g_energy[x][y] -= 0.3
-                #middle age
-                #the autumn years
-                g_energy[x][y] -= (0.04+0.02*g_power[x][y])
                 #death
                 if g_energy[x][y] > 1:
                     g_energy[x][y] = 1
