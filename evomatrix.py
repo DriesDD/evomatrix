0ce81444454715cfdf7b571bf1ae4b5f83b03a38
DriesDD <driesdedecker@gmail.com>
2020-10-08 10:23:39 +0200

    fix power penalty calculation

--- evomatrix.py
+++ evomatrix.py
@@ -1,14 +1,14 @@
 import Tkinter as grid
-import sched, time, random
+import sched, time, random, math
 
 g_cols, g_rows = 50,50
 g_alive  = [[False for x in range(g_cols)] for y in range(g_rows)]
 g_energy = [[0 for x in range(g_cols)] for y in range(g_rows)]
-g_motion = [[0.2 for x in range(g_cols)] for y in range(g_rows)]
-g_diet   = [[0 for x in range(g_cols)] for y in range(g_rows)]
-g_power   = [[0 for x in range(g_cols)] for y in range(g_rows)]
-tps = 100
-fps = 10
+g_motion = [[0.1 for x in range(g_cols)] for y in range(g_rows)]
+g_diet   = [[0.1 for x in range(g_cols)] for y in range(g_rows)]
+g_power   = [[0.1 for x in range(g_cols)] for y in range(g_rows)]
+tps = 40
+fps = 20
 
 root = grid.Tk()
 
@@ -16,8 +16,8 @@ g_alive[int(g_cols/2)][int(g_rows/2)] = True
 g_energy[int(g_cols/2)][int(g_rows/2)] = 0.8
 NEIGHBOURLIST = [[+1,0],[+1,+1],[0,+1],[-1,+1],[-1,0],[-1,-1],[0,-1],[+1,-1]]
 
-def floattohex(input): #helper function to map numbers from 0 to 1 to hex values from 0 to f
-    return str(hex(int(15*input)).split('x')[-1])
+def floattohex(input): #helper function to map numbers from 0 to 1 to hex values from 1 to f
+    return str(hex(int(16*input-1)).split('x')[-1])
 
 def mutate(n):
     return max(0, min(1, random.gauss(n,0.1)))
@@ -35,14 +35,13 @@ def draw_grid(): #draws every cell
                 c.create_rectangle(x*cs+trim,y*cs+trim,x*cs+cs-trim,y*cs+cs-trim,fill=cellcolor, tag='cell')
     root.after(1000/fps, draw_grid)
 
-
 def step_grid(): #calculations for behavior of every cell
     for x in range(0, g_cols):
         for y in range(0, g_rows):
             if g_alive[x][y]:
                 #growth and learning
                 g_energy[x][y] += 0.1 * (1 - g_diet[x][y])
-                #fighting each other (and moving)
+                #movement and fighting
                 if g_energy[x][y] > 0.2:
                     pick = random.choice(NEIGHBOURLIST)
                     ox = int(x+pick[0]) % g_cols
@@ -54,8 +53,8 @@ def step_grid(): #calculations for behavior of every cell
                             g_diet[ox][oy] = g_diet[x][y]
                             g_motion[ox][oy] = g_motion[x][y]
                             g_alive[x][y] = False
-                        else: #fight other
-                            if g_energy[ox][oy] > g_power[x][y]:
+                        else: #fighting each other and live organ transplants
+                            if g_energy[ox][oy] > g_power[x][y]: 
                                 g_energy[ox][oy] -= g_power[x][y]
                                 g_energy[x][y] += g_power[x][y] * g_diet[x][y]
                             else:
@@ -80,9 +79,8 @@ def step_grid(): #calculations for behavior of every cell
                             g_power[int(x+pick[0]) % g_cols][int(y+pick[1]) % g_rows] = mutate(g_power[x][y])                   
                             g_energy[x][y] -= 0.3
                 #middle age
-                #live organ transplants
                 #the autumn years
-                g_energy[x][y] -= 0.05-0.05*g_power[x][y]
+                g_energy[x][y] -= (0.04+0.02*g_power[x][y])
                 #death
                 if g_energy[x][y] > 1:
                     g_energy[x][y] = 1
